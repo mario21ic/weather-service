256a028b47dd340d18f6bbe16f38d700ea339552
Mario Inga <mario21ic@gmail.com>
2019-12-14 12:42:17 -0500

    Ready

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `joedayz`.`weather` (`city`, `description`, `icon`) VALUES ('Paris, France', 'Very cloudy!', 'weather-fog');
+INSERT INTO `joedayz`.`weather` (`city`, `description`, `icon`) VALUES ('London, UK', 'Quite cloudy', 'weather-pouring');
